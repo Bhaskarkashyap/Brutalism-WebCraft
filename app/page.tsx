09d104f099f1a254352437e38a61c2e587dcb18a
Bhaskarkashyap <bhaskarkashyap02@gmail.com>
2025-12-04 16:59:12 +0530

    customize default shadcn components for reusable use across pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <Button>Click</Button>
+    <div className="p-4 flex flex-col gap-y-5">
+      <div>
+        <Button variant={"elevated"}>Click</Button>
+      </div>
+      <div>
+        <Input />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="text" />
+      </div>
+    </div>
   );
 }
